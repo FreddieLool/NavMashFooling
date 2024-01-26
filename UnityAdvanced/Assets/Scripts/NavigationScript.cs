e012c8fd41a3cd4e22f6edd255eddbb3fd5459a4
FreddieLool <angrybird933@gmail.com>
2024-01-26 20:56:06 +0200

    LetsGoMurk

---
+++ UnityAdvanced/Assets/Scripts/NavigationScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationScript : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent navMeshAgent;
+    [SerializeField] Transform[] pathWaypoints;
+    [SerializeField] Animator animator;
+    
+    const string charSpeed = "Speed";
+
+    int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent.SetDestination(pathWaypoints[waypointIndex ].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat(charSpeed, navMeshAgent.velocity.magnitude);
+        if (!navMeshAgent.isStopped && navMeshAgent.remainingDistance <= 0.1f)
+        {
+            waypointIndex++;
+            if (waypointIndex >= pathWaypoints.Length)
+            {
+                waypointIndex = 0;
+            }
+
+            navMeshAgent.SetDestination(pathWaypoints[waypointIndex].position);
+        }
+    }
+}
